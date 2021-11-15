bf847b67a629683115914da00b2066081fa5b9c0
somekindofwallflower <someone.ana13@gmail.com>
2021-11-15 12:20:26 +0100

    Add an image to Welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,19 +18,9 @@
     @endif
 
     <div class="content">
+        <img src="/img/pizza.jpg" alt="pizza house logo" height="300px">
         <div class="title m-b-md">
-            Pizza House
-        </div>
-
-        <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
+            The North's Best Pizzas
         </div>
     </div>
 </div>
