f325a528a350c169872eca2d1a713e1644bc76f9
somekindofwallflower <someone.ana13@gmail.com>
2021-11-29 02:02:18 +0100

    Style fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                    <p><a href="{{route('pizzas.index')}}">View all pizza orders</a></p>
-                </div>
+                <h1 class="view-all-pizzas"><a style="color: white;" href="{{route('pizzas.index')}}">View all pizza orders</a></h1>
             </div>
         </div>
-    </div>
-</div>
 @endsection
